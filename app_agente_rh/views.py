ec4a3dcecee858afc9004140e8f05be83c11b332
Gabriel Henrique Abadi Ajala <gabrielajala1801@gmail.com>
2023-06-26 19:50:35 -0300

    Logindothtml adicionado

--- app_agente_rh/views.py
+++ app_agente_rh/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse #importado manualmente
+
 
 def login(request):
-    return HttpResponse('<h1>Bem-vindo a tela de login.</h1>')
+    return render(request, 'login.html')
