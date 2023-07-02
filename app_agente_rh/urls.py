bfb6de68087eb27fda0f0fad10f669413f3611ee
Gabriel Henrique Abadi Ajala <gabrielajala1801@gmail.com>
2023-07-02 17:01:28 -0300

    Criada estrutura de formularo + post

--- app_agente_rh/urls.py
+++ app_agente_rh/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from app_agente_rh.views import login
+from app_agente_rh.views import login, credenciais
 
 urlpatterns = [
-    path('', login),
+    path('', login, name="login"),
+    path('resultado/', credenciais, name="resultado")
 ]
--- app_agente_rh/views.py
+++ app_agente_rh/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def login(request):
     return render(request, 'login.html')
+
+def credenciais(request):
+    usuario = request.POST.get('usuario')
+    senha = request.POST.get('senha')
+    html = f"<h1>Usuário: {usuario} </br>Senha:{senha}</h1>"
+    return HttpResponse(html)
